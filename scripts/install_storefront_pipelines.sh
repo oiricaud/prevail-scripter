75c9fb7c0f369b52ca2d1fbf3b9c7e6831c5d4ee
Oscar Ricaud <Oscar.Ricaud@ibm.com>
2020-09-08 10:53:17 -0500

    Update install_storefront_pipelines.sh

--- scripts/install_storefront_pipelines.sh
+++ scripts/install_storefront_pipelines.sh
@@ -4,4 +4,4 @@ oc login -u admin -p passw0rd https://ocp.ibm.demo:8443
 oc project kabanero
 git clone https://github.com/ibm-garage-ref-storefront/devops-pipelines
 cd devops-pipelines
-oc apply --recursive --filename ./devops-pipelines/pipelines/stable/storefront-springboot
+oc apply --recursive --filename ./devops-pipelines/pipelines/stable/storefront-springboot/
