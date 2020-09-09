e98e5403dbe83a5346320be8b6c96d8f61b49973
oscar-ricaud <oscar-ricaud@ibm.com>
2020-09-09 20:57:22 +0000

    added modified files

---
+++ yaml/password.py
@@ -0,0 +1,10 @@
+import base64
+
+docker_username = base64.b64encode(b'')
+docker_password = base64.b64encode(b'')
+
+git_username = base64.b64encode(b'')
+
+print(docker_username)
+print(docker_password)
+print(git_username)
